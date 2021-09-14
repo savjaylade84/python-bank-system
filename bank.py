57436e90b0b049882a5b38f280ba18451d739eca
John Jayson B. De Leon <savjaylade84@gmail.com>
2021-09-14 16:51:41 +0800

    add comments

--- bank.py
+++ bank.py
@@ -61,6 +61,7 @@ class bank:
         print(self.balance)
         pass
 
+    #------------------- basic account functionality -----------------------------------
     def initReWriteBalance(self):
         #clear the list and supply it with fresh information
         self.info[2] = '{:n}'.format(self.balance)
@@ -84,6 +85,7 @@ class bank:
                 exit(1)
             index = index + 1
 
+        #collect all the info according to the user id
         with open(f'accounts/account-{self.user_id}.txt','r') as file:
             for data in file:
                 self.info.append(data.rstrip())
@@ -94,22 +96,28 @@ class bank:
         return False
 
     def initSignup(self):
+        #get the list of accounts in the system
         with open(f'accounts/account-numbers.txt','r') as file:
             for data in file:
                 self.account_numbers.append(data.rstrip())
 
+        #add account id accoording to the list
         self.user_id = f'000-000{len(self.account_numbers) + 1}' 
+        #get and add other user info 
         self.info.append(input('[Enter Name]:'))
         self.info.append(self.user_id)
         self.info.append(input('[Enter Amount]:'))
         self.info.append(input('[Enter Password]:'))
+        #write the user info in the new file
         with open(f'accounts/account-{self.user_id}.txt','w') as file:
                 for data in self.info:
                     file.write(f'{data}\n')
+        #update the account list
         with open(f'accounts/account-numbers.txt','a') as file:
             file.write(f'\n{self.info[0]}|{self.user_id}')
         print("==========[Successful Login!]==========")
 
+    #clear everything
     def initClear(self):
         self.account_numbers = []
         self.path = ""
